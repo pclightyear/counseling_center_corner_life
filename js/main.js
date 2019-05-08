7af79a732588ed59bb18453be9726d74bec2a57e
pclightyear <ee91608@gmail.com>
2019-05-08 17:54:39 +0800

    v0.0.1 layer 1 add fade in/out effect

--- js/main.js
+++ js/main.js
@@ -1,30 +1,55 @@
 $('#main-img').css('margin-top', $('#top-navbar').outerHeight());
 
+var intervaltimes = 1000;
+
 $('#btn_dark').on({
     'click': function(){
-        console.log($(".current-img").length);
-        $(".current-img").removeClass( "current-img" ).addClass( "hidden" );
-        $('#dark-img').removeClass( "hidden" ).addClass( "current-img" );
+        $(".current-img").fadeOut(intervaltimes);
+        maskInterval = setInterval(() => {
+            clearInterval(maskInterval); 
+            $("#dark-img").fadeIn(intervaltimes);
+            $(".current-img").removeClass( "current-img" );
+            $('#dark-img').addClass( "current-img" );
+            $("#dark-img").css("display", "block");
+        }, intervaltimes);
     }
 });
 
 $('#btn_accomp').on({
     'click': function(){
-        $('.current-img').removeClass( "current-img" ).addClass( "hidden" );
-        $('#accomp-img').removeClass( "hidden" ).addClass( "current-img" );
+        $(".current-img").fadeOut(intervaltimes);
+        maskInterval = setInterval(() => {
+            clearInterval(maskInterval); 
+            $("#accomp-img").fadeIn(intervaltimes);
+            $(".current-img").removeClass( "current-img" );
+            $('#accomp-img').addClass( "current-img" );
+            $("#accomp-img").css("display", "block");
+        }, intervaltimes);
     }
 });
 
 $('#btn_conf').on({
     'click': function(){
-        $('.current-img').removeClass( "current-img" ).addClass( "hidden" );
-        $('#conf-img').removeClass( "hidden" ).addClass( "current-img" );
+        $(".current-img").fadeOut(intervaltimes);
+        maskInterval = setInterval(() => {
+            clearInterval(maskInterval); 
+            $("#conf-img").fadeIn(intervaltimes);
+            $(".current-img").removeClass( "current-img" );
+            $('#conf-img').addClass( "current-img" );
+            $("#conf-img").css("display", "block");
+        }, intervaltimes);
     }
 });
 
 $('#btn_lookfor').on({
     'click': function(){
-        $('.current-img').removeClass( "current-img" ).addClass( "hidden" );
-        $('#lookfor-img').removeClass( "hidden" ).addClass( "current-img" );
+        $(".current-img").fadeOut(intervaltimes);
+        maskInterval = setInterval(() => {
+            clearInterval(maskInterval); 
+            $("#lookfor-img").fadeIn(intervaltimes);
+            $(".current-img").removeClass( "current-img" );
+            $('#lookfor-img').addClass( "current-img" );
+            $("#lookfor-img").css("display", "block");
+        }, intervaltimes);
     }
 });
