980460a00795a4ae398bfd3178f1f3002e9c0c64
pclightyear <ee91608@gmail.com>
2019-05-25 23:09:36 +0800

    v0.1.0 layer1 finish

--- js/main.js
+++ js/main.js
@@ -1,61 +1,7 @@
-$('#main-img').css('margin-top', $('#top-navbar').outerHeight());
-
-var intervaltimes = 1000;
-
 $.when( $.ready ).then(function() {
+    var ran_idx = Math.floor(Math.random() * 4);
+    $("#url" + ran_idx).addClass('active');
     if(window.innerWidth <= 1200) {
         alert("請將畫面橫擺以獲得較佳的觀看體驗");
-    }
-});
-
-$('#btn_dark').on({
-    'click': function(){
-        $(".current-img").fadeOut(intervaltimes);
-        maskInterval = setInterval(() => {
-            clearInterval(maskInterval); 
-            $("#dark-img").fadeIn(intervaltimes);
-            $(".current-img").removeClass( "current-img" );
-            $('#dark-img').addClass( "current-img" );
-            $("#dark-img").css("display", "block");
-        }, intervaltimes);
-    }
-});
-
-$('#btn_accomp').on({
-    'click': function(){
-        $(".current-img").fadeOut(intervaltimes);
-        maskInterval = setInterval(() => {
-            clearInterval(maskInterval); 
-            $("#accomp-img").fadeIn(intervaltimes);
-            $(".current-img").removeClass( "current-img" );
-            $('#accomp-img').addClass( "current-img" );
-            $("#accomp-img").css("display", "block");
-        }, intervaltimes);
-    }
-});
-
-$('#btn_conf').on({
-    'click': function(){
-        $(".current-img").fadeOut(intervaltimes);
-        maskInterval = setInterval(() => {
-            clearInterval(maskInterval); 
-            $("#conf-img").fadeIn(intervaltimes);
-            $(".current-img").removeClass( "current-img" );
-            $('#conf-img').addClass( "current-img" );
-            $("#conf-img").css("display", "block");
-        }, intervaltimes);
-    }
-});
-
-$('#btn_lookfor').on({
-    'click': function(){
-        $(".current-img").fadeOut(intervaltimes);
-        maskInterval = setInterval(() => {
-            clearInterval(maskInterval); 
-            $("#lookfor-img").fadeIn(intervaltimes);
-            $(".current-img").removeClass( "current-img" );
-            $('#lookfor-img').addClass( "current-img" );
-            $("#lookfor-img").css("display", "block");
-        }, intervaltimes);
     }
 });
